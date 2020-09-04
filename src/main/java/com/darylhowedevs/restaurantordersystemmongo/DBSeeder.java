a41da2cf9fbc6288010e8024cdea76b238ddd307
DarylHowe <howedaryl@hotmail.com>
2020-09-04 14:21:23 +0100

    DB Seeder refactored

--- src/main/java/com/darylhowedevs/restaurantordersystemmongo/DBSeeder.java
+++ src/main/java/com/darylhowedevs/restaurantordersystemmongo/DBSeeder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -19,144 +20,139 @@ import com.darylhowedevs.restaurantordersystemmongo.order.Order;
 import com.darylhowedevs.restaurantordersystemmongo.server.Server;
 import com.darylhowedevs.restaurantordersystemmongo.server.ServerRepository;
 
+/**
+ * DB Seeder - This class seeds MongoDB with data. Each time the application
+ * restarts all data is removed and re-seeded to the initial seed state.
+ */
 @Component
 public class DBSeeder implements CommandLineRunner {
 
+	@Autowired
 	private MenuRepository menuRepository;
+	@Autowired
 	private ServerRepository serverRepository;
+	@Autowired
 	private KitchenRepository kitchenRepository;
 
-
-	public DBSeeder(MenuRepository menuRepository, ServerRepository serverRepository, KitchenRepository kitchenRepository) {
-		this.menuRepository = menuRepository;
-		this.serverRepository = serverRepository;
-		this.kitchenRepository = kitchenRepository;
-	}
+	private ArrayList<Menu> menusList = new ArrayList<>();
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		seedMenuData();
+		seedServerData();
+		seedKitchenData();
+	}
+
+	/**
+	 * A method which seeds 4 menus and seeds items into menu. 
+	 */
+	private void seedMenuData() {
 		menuRepository.deleteAll();
 
-		Menu menu01 = new Menu("Main");
-		Item item01 = new Main("Pizza", 12.95);
-		Item item02 = new Main("Beef Burger", 15.65);
-		Item item03 = new Main("Lasange", 13.00);
-		Item item04 = new Main("Chicken Burger", 16.45);
-		Item item05 = new Main("Chicken Curry", 18.99);
-		Item item06 = new Main("Fillet Steak", 25.50);
-		Item item07 = new Main("Seafood Paella", 21.99);
+		menusList.add(new Menu("Main"));
+		menusList.get(0).addItemToMenu(new Main("Pizza", 12.95));
+		menusList.get(0).addItemToMenu(new Main("Beef Burger", 15.65));
+		menusList.get(0).addItemToMenu(new Main("Lasange", 13.00));
+		menusList.get(0).addItemToMenu(new Main("Chicken Burger", 16.45));
+		menusList.get(0).addItemToMenu(new Main("Chicken Curry", 18.99));
+		menusList.get(0).addItemToMenu(new Main("Fillet Steak", 25.50));
+		menusList.get(0).addItemToMenu(new Main("Seafood Paella", 21.99));
 
-		menu01.addItemToMenu(item01);
-		menu01.addItemToMenu(item02);
-		menu01.addItemToMenu(item03);
-		menu01.addItemToMenu(item04);
-		menu01.addItemToMenu(item05);
-		menu01.addItemToMenu(item06);
-		menu01.addItemToMenu(item07);
+		menusList.add(new Menu("Starter"));
+		menusList.get(1).addItemToMenu(new Starter("Chicken Wings", 7.50));
+		menusList.get(1).addItemToMenu(new Starter("Garlic Bread", 2.85));
+		menusList.get(1).addItemToMenu(new Starter("Cheese Platter", 9.50));
+		menusList.get(1).addItemToMenu(new Starter("Tomato Soup", 4.99));
+		menusList.get(1).addItemToMenu(new Starter("Bruschetta", 5.60));
+		menusList.get(1).addItemToMenu(new Starter("Mussels", 8.95));
+		menusList.get(1).addItemToMenu(new Starter("Prawn Pil Pil", 11.20));
 
-		Menu menu02 = new Menu("Starter");
-		Item item08 = new Starter("Chicken Wings", 7.50);
-		Item item09 = new Starter("Garlic Bread", 2.85);
-		Item item10 = new Starter("Cheese Platter", 9.50);
-		Item item11 = new Starter("Tomato Soup", 4.99);
-		Item item12 = new Starter("Bruschetta", 5.60);
-		Item item13 = new Starter("Mussels", 8.95);
-		Item item14 = new Starter("Prawn Pil Pil", 11.20);
+		menusList.add(new Menu("Drinks"));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("Water", 0));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("Coke", 2.50));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("7UP", 2.50));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("Fanta", 2.50));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("Milk", 1.00));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("Lemonade", 2.50));
+		menusList.get(2).addItemToMenu(new DrinkNonAlcohol("Ribenna", 2.50));
 
-		menu02.addItemToMenu(item08);
-		menu02.addItemToMenu(item09);
-		menu02.addItemToMenu(item10);
-		menu02.addItemToMenu(item11);
-		menu02.addItemToMenu(item12);
-		menu02.addItemToMenu(item13);
-		menu02.addItemToMenu(item14);
+		menusList.add(new Menu("Alcohol"));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Red Wine Small", 8.50));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Red Wine Large", 11.50));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Heiniken Pint", 5.50));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Guiness Pint", 4.50));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Coors Pint", 5.00));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Budweiser Pint", 5.50));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Jameson Single", 7.50));
+		menusList.get(3).addItemToMenu(new DrinkAlcohol("Vodka Single", 6.50));
 
-		Menu menu03 = new Menu("Drinks");
-		Item item15 = new DrinkNonAlcohol("Water", 0);
-		Item item16 = new DrinkNonAlcohol("Coke", 2.50);
-		Item item17 = new DrinkNonAlcohol("7UP", 2.50);
-		Item item18 = new DrinkNonAlcohol("Fanta", 2.50);
-		Item item19 = new DrinkNonAlcohol("Milk", 1.00);
-		Item item20 = new DrinkNonAlcohol("Lemonade", 2.50);
-		Item item21 = new DrinkNonAlcohol("Ribenna", 2.50);
-
-		menu03.addItemToMenu(item15);
-		menu03.addItemToMenu(item16);
-		menu03.addItemToMenu(item17);
-		menu03.addItemToMenu(item18);
-		menu03.addItemToMenu(item19);
-		menu03.addItemToMenu(item20);
-		menu03.addItemToMenu(item21);
-
-		Menu menu04 = new Menu("Alcohol");
-		Item item22 = new DrinkAlcohol("Red Wine Small", 8.50);
-		Item item23 = new DrinkAlcohol("Red Wine Large", 11.50);
-		Item item24 = new DrinkAlcohol("Heiniken Pint", 5.50);
-		Item item25 = new DrinkAlcohol("Guiness Pint", 4.50);
-		Item item26 = new DrinkAlcohol("Coors Pint", 5.00);
-		Item item27 = new DrinkAlcohol("Budweiser Pint", 5.50);
-		Item item28 = new DrinkAlcohol("Jameson Single", 7.50);
-		Item item29 = new DrinkAlcohol("Vodka Single", 6.50);
-
-		menu04.addItemToMenu(item22);
-		menu04.addItemToMenu(item23);
-		menu04.addItemToMenu(item24);
-		menu04.addItemToMenu(item25);
-		menu04.addItemToMenu(item26);
-		menu04.addItemToMenu(item27);
-		menu04.addItemToMenu(item28);
-		menu04.addItemToMenu(item29);
-
-		List<Menu> menuList = Arrays.asList(menu01, menu02, menu03, menu04);
+		List<Menu> menuList = Arrays.asList(menusList.get(0), menusList.get(1), menusList.get(2), menusList.get(3));
 		menuRepository.saveAll(menuList);
-		
+	}
+
+	/**
+	 * A method which seeds 3 servers. Server 'Daryl Howe' seeded with 1 closed order and 1 open order. 
+	 */
+	private void seedServerData() {
+
 		serverRepository.deleteAll();
-		
+
 		Server server01 = new Server("Daryl Howe");
 		Server server02 = new Server("Sandy Luc");
 		Server server03 = new Server("Paul Rodgers");
-		
-		List<Item> itemList = new ArrayList<>();
-		itemList.add(item04);
-		itemList.add(item12);
-		itemList.add(item29);
 
-		Order order01 = new Order(10, itemList);
+		List<Item> itemList01 = new ArrayList<>();
+		itemList01.add(menusList.get(0).getItemByIndex(3));
+		itemList01.add(menusList.get(1).getItemByIndex(3));
+		itemList01.add(menusList.get(3).getItemByIndex(1));
+
+		List<Item> itemList02 = new ArrayList<>();
+		itemList02.add(menusList.get(0).getItemByIndex(1));
+		itemList02.add(menusList.get(0).getItemByIndex(5));
+		itemList02.add(menusList.get(2).getItemByIndex(5));
+		itemList02.add(menusList.get(3).getItemByIndex(1));
+		itemList02.add(menusList.get(3).getItemByIndex(1));
+
+		Order order01 = new Order(10, itemList01);
+		Order order02 = new Order(20, itemList02);
+
 		server01.createOrder(order01);
 		server01.closeOrderByTableNumber(10);
-		
-		List<Item> itemList02 = new ArrayList<>();
-		itemList02.add(item03);
-		itemList02.add(item14);
-		itemList02.add(item26);
 
-		Order order02 = new Order(20, itemList02);
 		server01.createOrder(order02);
-		
+
 		List<Server> serverList = Arrays.asList(server01, server02, server03);
 		serverRepository.saveAll(serverList);
-		
-		
-		// **************** KITCHEN 
-		
+	}
+
+	/**
+	 * A method which seeds data to the kitchen. 2 orders are seeded one of which 'isReady' to be served.
+	 */
+	private void seedKitchenData() {
+
 		kitchenRepository.deleteAll();
-		
-		List<Item> itemList03 = new ArrayList<>();
-		itemList03.add(item10);
-		itemList03.add(item12);
-		itemList03.add(item11);
-		Order order03 = new Order(33, itemList03);
-		
-		List<Item> itemList04 = new ArrayList<>();
-		itemList04.add(item03);
-		itemList04.add(item22);
-		itemList04.add(item19);
-		Order order04 = new Order(41, itemList04);
-		order04.setIsReadyToBeServed(true);
-		
-		kitchenRepository.save(order03);
-		kitchenRepository.save(order04);
-		
+
+		List<Item> itemList = new ArrayList<>();
+		itemList.add(menusList.get(0).getItemByIndex(4));
+		itemList.add(menusList.get(0).getItemByIndex(3));
+		itemList.add(menusList.get(0).getItemByIndex(3));
+		itemList.add(menusList.get(3).getItemByIndex(1));
+		itemList.add(menusList.get(3).getItemByIndex(5));
+
+		List<Item> itemList01 = new ArrayList<>();
+		itemList01.add(menusList.get(1).getItemByIndex(4));
+		itemList01.add(menusList.get(1).getItemByIndex(3));
+		itemList01.add(menusList.get(0).getItemByIndex(1));
+		itemList01.add(menusList.get(3).getItemByIndex(4));
+		itemList01.add(menusList.get(3).getItemByIndex(6));
+
+		Order order01 = new Order(33, itemList);
+		Order order02 = new Order(41, itemList01);
+
+		order01.setIsReadyToBeServed(true);
+
+		kitchenRepository.save(order01);
+		kitchenRepository.save(order02);
 	}
 }
